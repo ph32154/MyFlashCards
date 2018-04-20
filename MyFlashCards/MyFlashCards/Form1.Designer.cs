ee439d3ea2341e45ab1079661b0ed667e606063c
ph32154 <ph32154@hotmail.com>
2018-04-20 15:26:54 +0100

    Add files via upload
    Initial backup of my flashcards program

---
+++ MyFlashCards/MyFlashCards/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace MyFlashCards
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblLeft = new System.Windows.Forms.Label();
+            this.lblRight = new System.Windows.Forms.Label();
+            this.btnCorrect = new System.Windows.Forms.Button();
+            this.btnWrong = new System.Windows.Forms.Button();
+            this.lblScoreTxt = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.btnShowAnswer = new System.Windows.Forms.Button();
+            this.lblEnd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(211, 26);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(127, 32);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Title here";
+            // 
+            // lblLeft
+            // 
+            this.lblLeft.AutoSize = true;
+            this.lblLeft.Location = new System.Drawing.Point(26, 116);
+            this.lblLeft.Name = "lblLeft";
+            this.lblLeft.Size = new System.Drawing.Size(92, 13);
+            this.lblLeft.TabIndex = 1;
+            this.lblLeft.Text = "left hand contents";
+            // 
+            // lblRight
+            // 
+            this.lblRight.AutoSize = true;
+            this.lblRight.Location = new System.Drawing.Point(298, 114);
+            this.lblRight.Name = "lblRight";
+            this.lblRight.Size = new System.Drawing.Size(103, 13);
+            this.lblRight.TabIndex = 2;
+            this.lblRight.Text = "Right hand contents";
+            // 
+            // btnCorrect
+            // 
+            this.btnCorrect.Location = new System.Drawing.Point(21, 293);
+            this.btnCorrect.Name = "btnCorrect";
+            this.btnCorrect.Size = new System.Drawing.Size(122, 38);
+            this.btnCorrect.TabIndex = 3;
+            this.btnCorrect.Text = "Got it right";
+            this.btnCorrect.UseVisualStyleBackColor = true;
+            this.btnCorrect.Click += new System.EventHandler(this.btnCorrect_Click);
+            // 
+            // btnWrong
+            // 
+            this.btnWrong.Location = new System.Drawing.Point(149, 293);
+            this.btnWrong.Name = "btnWrong";
+            this.btnWrong.Size = new System.Drawing.Size(122, 38);
+            this.btnWrong.TabIndex = 4;
+            this.btnWrong.Text = "Got it wrong";
+            this.btnWrong.UseVisualStyleBackColor = true;
+            this.btnWrong.Click += new System.EventHandler(this.btnWrong_Click);
+            // 
+            // lblScoreTxt
+            // 
+            this.lblScoreTxt.AutoSize = true;
+            this.lblScoreTxt.Location = new System.Drawing.Point(427, 298);
+            this.lblScoreTxt.Name = "lblScoreTxt";
+            this.lblScoreTxt.Size = new System.Drawing.Size(61, 13);
+            this.lblScoreTxt.TabIndex = 5;
+            this.lblScoreTxt.Text = "Your score:";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(505, 298);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(33, 13);
+            this.lblScore.TabIndex = 6;
+            this.lblScore.Text = "blank";
+            // 
+            // btnShowAnswer
+            // 
+            this.btnShowAnswer.Location = new System.Drawing.Point(21, 247);
+            this.btnShowAnswer.Name = "btnShowAnswer";
+            this.btnShowAnswer.Size = new System.Drawing.Size(121, 35);
+            this.btnShowAnswer.TabIndex = 8;
+            this.btnShowAnswer.Text = "Show Answer";
+            this.btnShowAnswer.UseVisualStyleBackColor = true;
+            this.btnShowAnswer.Click += new System.EventHandler(this.btnShowAnswer_Click);
+            // 
+            // lblEnd
+            // 
+            this.lblEnd.AutoSize = true;
+            this.lblEnd.Location = new System.Drawing.Point(41, 151);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(110, 13);
+            this.lblEnd.TabIndex = 9;
+            this.lblEnd.Text = "Reached the end text";
+            this.lblEnd.Visible = false;
+            this.lblEnd.Click += new System.EventHandler(this.lblEnd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 355);
+            this.Controls.Add(this.lblEnd);
+            this.Controls.Add(this.btnShowAnswer);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblScoreTxt);
+            this.Controls.Add(this.btnWrong);
+            this.Controls.Add(this.btnCorrect);
+            this.Controls.Add(this.lblRight);
+            this.Controls.Add(this.lblLeft);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Flash Card Program";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblLeft;
+        private System.Windows.Forms.Label lblRight;
+        private System.Windows.Forms.Button btnCorrect;
+        private System.Windows.Forms.Button btnWrong;
+        private System.Windows.Forms.Label lblScoreTxt;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Button btnShowAnswer;
+        private System.Windows.Forms.Label lblEnd;
+    }
+}
+
---
+++ MyFlashCards/MyFlashCards/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFlashCards
+{
+    public partial class Form1 : Form
+    {
+        //global declarations
+        Pair displayPair = new Pair();
+        int score = 0;
+        int position = 0;
+        List<Pair> storedPairs;
+
+
+        /*
+              To Do:
+              1) 
+              2) Read in the pairs list from a .csv file
+              3) randomize the pairs so you don't know what's coming next.
+              4) Add in "start again" button when we reach the end of the list.  It should randomize the list and reset the score.
+              5) pretty up the display
+
+
+        */
+
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            lblTitle.Text = "Test Framework";
+            lblScore.Text = score.ToString();
+
+            // initialise the list - will need to get this from a .csv later
+
+            storedPairs = new List<Pair>
+            {
+                new Pair() {LHS = "hola", RHS = "hello" },
+                new Pair() {LHS = "mundo", RHS = "world" }
+            };
+
+            storedPairs.Add(new Pair() { LHS = "playa", RHS = "beach" });
+
+            
+            //now transfer the stored pairs deck to the actual display
+            displayPair.LHS = storedPairs[position].LHS;
+            displayPair.RHS = storedPairs[position].RHS;
+            lblTitle.Text = "test1";
+
+            //initialise the screen
+            lblLeft.Text = displayPair.LHS;
+            lblRight.Text = "What's the answer?";
+            btnCorrect.Enabled = false;
+            btnWrong.Enabled = false;
+
+        }
+
+        private void btnCorrect_Click(object sender, EventArgs e)
+        {
+            score += 1;
+            moveOn();
+        }
+
+        private void btnWrong_Click(object sender, EventArgs e)
+        {
+
+            moveOn();
+        }
+
+        private void moveOn()
+        {
+            lblScore.Text = score.ToString();
+            
+            btnCorrect.Enabled = false;
+            btnWrong.Enabled = false;
+            UpdateDisplay();
+
+        }
+
+        private void UpdateDisplay()
+        {
+            //show answer on right, move on to next pair, display next question on left.
+
+            position += 1;
+            nextPair();
+            lblLeft.Text = displayPair.LHS;
+            lblRight.Text = "What's the answer?";
+            btnShowAnswer.Enabled = true;
+        }
+
+        private void nextPair()
+        {
+            
+            if (position < storedPairs.Count)
+            {
+                displayPair.LHS = storedPairs[position].LHS;
+                displayPair.RHS = storedPairs[position].RHS;
+            }
+            else
+            {
+                string strFinalText = "You reached the end of the list.  You scored " + score.ToString() + " out of " + storedPairs.Count;
+                lblEnd.Text = strFinalText;
+                lblEnd.Visible = true;
+                displayPair.LHS = "end of list";
+                displayPair.RHS = "end of list";
+            } 
+
+        }
+
+
+        private void btnShowAnswer_Click(object sender, EventArgs e)
+        {
+            lblRight.Text = displayPair.RHS;
+            btnCorrect.Enabled = true;
+            btnWrong.Enabled = true;
+            btnShowAnswer.Enabled = false;
+        }
+
+        private void lblEnd_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class Pair
+    {
+        public string LHS { get; set; }
+        public string RHS { get; set; }
+
+    }//end class pair
+
+        
+
+
+}
